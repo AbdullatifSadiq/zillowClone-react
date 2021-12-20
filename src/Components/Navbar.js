c90fb95b35e74bbab8913afc95fdac4c9a0e6db2
AbdullatifSadiq <abdul.latif708@yahoo.com>
2021-12-20 16:42:16 +0800

    worked on data.js and index.css

---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React,{useState,useEffect,useRef} from 'react'
+import {Links} from '../data';
+
+const Navbar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/data.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+export const Links = [
+    {
+        id:1,
+        nav:'Buy',
+        url:'/'
+    },
+    {
+        id:2,
+        nav:'Sell',
+        url:'/'
+    },
+    {
+        id:3,
+        nav:'Rent',
+        url:'/'
+    }, {
+        id:4,
+        nav:'Home Loans',
+        url:'/'
+    },
+    {
+        id:5,
+        nav:'Agent Finder',
+        url:'/'
+    },
+    {
+        id:6,
+        nav:'Manage Rentals',
+        url:'/'
+    },
+    {
+        id:7,
+        nav:'Advertise',
+        url:'/'
+    },
+    {
+        id:8,
+        nav:'Help',
+        url:'/'
+    }, {
+        id:9,
+        nav:'Sign In',
+        url:'/'
+    },
+]
