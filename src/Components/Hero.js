9e506bc1427d2690ad2622b103b998ac888867e7
AbdullatifSadiq <abdul.latif708@yahoo.com>
2021-12-21 16:33:03 +0000

    started working on the hero section

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import { FaSearch } from 'react-icons/fa';
 
 const Hero = () => {
   return (
     <section className='hero'>
       <div className='center-content'>
         <h3>Change starts here</h3>
-        <div className='search-box'>
-          <input className='search-box' type='text' id />
+        <div className='search'>
+          <input
+            className='search-box'
+            type='text'
+            placeholder='Address, neighborhood, city, ZIP'
+          />
+          <span
+            className='search-icon'
+            style={{
+              background: '#fff',
+              width: '17%',
+              height: '3.4rem',
+              borderTopRightRadius: '0.3rem',
+              borderEndEndRadius: '0.3rem',
+            }}
+          >
+            <FaSearch />
+          </span>
         </div>
       </div>
     </section>
