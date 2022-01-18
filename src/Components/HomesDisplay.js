65d9c4c4220c792f61e4b786c0265f988f66f44e
Abdul-latif Sadiq <abdul.latif708@yahoo.com>
2022-01-18 16:32:00 +0000

    nothing major

--- src/Components/HomesDisplay.js
+++ src/Components/HomesDisplay.js
@@ -6,7 +6,7 @@ const HomesDisplay = () => {
       <div className='empty-space'></div>
       <div className='search-nav'>search nav</div>
       <div className='variables-nav'>variable's nav</div>
-      <div className='homes'></div>
+      <div className='homes'>Home Components</div>
     </>
   );
 };
