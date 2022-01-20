c5f0c26daf1f7203f83e172c8d180841d21d56e1
Abdul-latif Sadiq <abdul.latif708@yahoo.com>
2022-01-20 13:31:16 +0000

    doen with search-nav ui

--- src/Components/HomesDisplay.js
+++ src/Components/HomesDisplay.js
@@ -1,10 +1,53 @@
 import React from 'react';
+import { FaBars } from 'react-icons/fa';
 
 const HomesDisplay = () => {
   return (
     <>
-      <div className='empty-space'></div>
-      <div className='search-nav'>search nav</div>
+      <div className='search-nav'>
+        <button className='nav-toggle'>
+          <FaBars
+            style={{
+              color: '#006aff',
+              fontSize: '20px',
+            }}
+            className='nav-toggle'
+            // onClick={() => setShowLinks(true)}
+          />
+        </button>
+        <span>
+          <img
+            style={{
+              alignSelf: 'center',
+              height: '25px',
+              paddingInline: '0.5rem',
+            }}
+            src='https://s.zillowstatic.com/pfs/static/z-logo-icon.svg'
+            alt='zillow logo'
+          />
+        </span>
+        {/* <div> */}
+        <input
+          className='search-box-homes'
+          type='text'
+          placeholder='Location'
+          style={{ height: '34px' }}
+        />
+        {/* </div> */}
+        <span>
+          <a
+            href='/'
+            className='links'
+            style={{
+              color: '#006aff',
+              fontSize: '0.9rem',
+              paddingInline: '0.6rem',
+            }}
+          >
+            Sign In
+          </a>
+        </span>
+      </div>
       <div className='variables-nav'>variable's nav</div>
       <div className='homes'>Home Components</div>
     </>
